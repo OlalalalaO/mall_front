f57fbb1d8e1f3e56925f6a999c58c2c86e424e75
oalalalao <1176328868@qq.com>
2022-01-28 04:20:22 +0800

    first

---
+++ src/views/product/category.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "category",
+  created() {
+    this.$axios({
+      method: 'get',
+      url: 'http://localhost:8086/api/product/category/'
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
